c5dd89e41cba80fe705f9ece046f8316c66806c2
brayden09876 <bij0004@mix.wvu.edu>
2023-01-11 14:31:22 -0500

    Brayden Jett Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,27 @@
 install.packages("swirl")
 library(swirl)
+swirl()
+Brayden
+1
+1
+1
+1
+5 + 7
+x <- 5 + 7
+x
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+1
+my_sqrt
+my_div <- z / my_sqrt
+1
+my_div
+c(1, 2, 3, 4) + c(0, 10, 100)
+2
+
