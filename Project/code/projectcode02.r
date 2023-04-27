2cd5be4300c8f2e699a3b2c4b84fbf8c15babb65
brayden09876 <bij0004@mix.wvu.edu>
2023-04-27 10:03:47 -0400

    Task 10

--- Project/code/projectcode02.r
+++ Project/code/projectcode02.r
@@ -10,3 +10,50 @@ New_Data$length
 New_Data$weight
 New_Data$year
 New_Data$year <- factor(New_Data$year, levels = c(1982, 1986, 1991, 1992, 2012, 2014, 2015, 2016))
+eel_test <- brm(bf(length ~ (1 | year), sigma ~ (1 | year)),
+    data = New_Data, family = gaussian())
+eel_test
+plot(eel_test)
+weight_test <- brm(bf(weight ~ (1 | year), sigma ~ (1 | year)),
+                   data = New_Data, family = gaussian())
+weight_test
+plot(weight_test)
+LengthEffects <- ranef(eel_test)
+WeightEffects <- ranef(weight_test)
+
+# bayesplot & stanplot packages for pretty plots.
+# you care about the random effect
+plot(c(1982, 1986, 1991, 1992, 2012, 2014, 2015, 2016), LengthEffects$year[,1,1], type="b")
+plot(c(1982, 1986, 1991, 1992, 2012, 2014, 2015, 2016), WeightEffects$year[,1,1], type="b")
+
+library(magrittr)
+library(dplyr)
+library(purrr)
+library(forcats)
+library(tidyr)
+library(modelr)
+library(ggdist)
+library(tidybayes)
+library(ggplot2)
+library(cowplot)
+library(rstan)
+library(brms)
+library(ggrepel)
+library(RColorBrewer)
+library(gganimate)
+library(posterior)
+library(distributional)
+
+theme_set(theme_tidybayes() + panel_border())
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+New_Data %>%
+  data_grid(year) %>%
+  add_epred_draws(eel_test, dpar = c("mu", "sigma")) %>%
+  sample_draws(30) %>%
+  ggplot(aes(y = year)) +
+  stat_slab(aes(xdist = dist_normal(mu, sigma)), 
+            slab_color = "gray65", alpha = 1/10, fill = NA
+  ) +
+  geom_point(aes(x = length), shape = 21, fill = "#9ECAE1", size = 2)
---
+++ Task_10/task10.r
@@ -0,0 +1,30 @@
+setwd('~/Desktop/Evolution/Tasks/Task_10')
+x <- rnorm(n = 100, mean = 0, sd = 2)
+x
+y <- x*5+2+runif(100, min = 0, max = 0.1)
+y
+linmodel <- lm(y~x)
+plot(linmodel)
+slope <- vector("numeric", 100)
+intercept <- vector("numeric", 100)
+z <- vector("numeric", 100)
+for (i in 1:100) {
+  x <- rnorm(100, mean = 0, sd = 2)
+  z[i] <- runif(1, min = 0.5, max = 2)
+  y <- x*5+2+runif(100, min = 0, max = 0.1)
+  linearmodel <- lm(y~x)
+  intercept[i] <- coef(linearmodel)[1]*z[i]+2
+  slope[i] <- coef(linearmodel)[2]*z[i]
+}
+plot(z, slope, xlab = "Z Estimated Slope")
+abline(lm(slope~z), col = "red")
+n <- 10000
+prize <- sample(c("1", "2", "3"), size = n, replace = TRUE)
+opened_door <- ifelse(prize == "1", sample(c("2", "3"), size = n, replace = TRUE), ifelse(prize == "2", "3", "2"))
+closed_door <- ifelse(opened_door == "2", "3", "2")
+same_door <- sum(prize =="1")/n
+diff_door <- sum(prize == closed_door)/n
+win_frequency <- c(same_door, diff_door)
+barplot(win_frequency, names.arg = c("Same Door", "Different Door"), ylab ="Frequency of Wins (10,000 Runs)", ylim = c(0, 0.8), main = "Chance of Winning the Grand Prize", col = "lightpink")
+u <- "https://pyxis.nymag.com/v1/imgs/2e7/5d7/73e020f56c4a86221823bc32113b4316d2-25-ben-affleck-sad-smoke.w710.jpg"
+meme(u, "", "Me after taking one of Dr. Mitchell's exams")
